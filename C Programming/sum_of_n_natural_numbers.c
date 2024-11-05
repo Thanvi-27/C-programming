2a9030dc3d6586e34625dd38797641c1e226621b
Thanvi-27 <thanvikummithi@gmail.com>
2024-11-05 23:42:07 +0530

    updated the code

--- C Programming/sum_of_n_natural_numbers.c
+++ C Programming/sum_of_n_natural_numbers.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main() {
     int n;
-    int sum=0;
     printf("enter a number:");
     scanf("%d",&n);
+
+    int sum=0;
     for(int i=0;i<=n;i++) {
         sum=sum+i;
     }
+
     printf("sum is %d",sum);
 }
