b05d17596260c5124fa19ecc34ee137e849a9aed
rajamohank <rajamohank@gmail.com>
2024-11-05 10:57:47 -0600

    Updated the code with better formatting - 1

--- C Programming/calculator_using_switchcase.c
+++ C Programming/calculator_using_switchcase.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
+
 int main() {
-    int a;
-    int b;
+    int a, b;
+    
     printf("enter a value:");
     scanf("%d",&a);
+
     printf("enter b value:");
     scanf("%d",&b);
+
     int n;
     printf("enter a case:");
     scanf("%d",&n);
-    int sum;
-    sum=a+b;
-    int diff;
-    diff=a-b;
-    int mult;
-    mult=a*b;
-    float div;
-    div=a/b;
+
+    int sum = sum=a+b;
+    int diff = diff=a-b;
+    int mult = a*b;
+    float div = a/b;
+
     switch(n)
     {
         case 1:
         printf("%d+%d=%d",a,b,sum);
         break;
+
         case 2:
         printf("%d-%d=%d",a,b,diff);
         break;
+
         case 3:
         printf("%d*%d=%d",a,b,mult);
+        break;
+
         case 4:
         printf("%d/%d=%f",a,b,div);
+        break;
+
         default:
         printf("entered a wrong operator");
     }
