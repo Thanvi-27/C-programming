774492f3c71f2b7c3bcefa7a4f26ab0d02500de4
Thanvi-27 <thanvikummithi@gmail.com>
2024-11-06 23:18:06 +0530

    File

---
+++ C Programming/palindrome_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int num,rev=0,rem;
+    printf("enter a number:");
+    scanf("%d",&num);
+    while(num>0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+
+    }
+    if(num==rev){
+        printf("%d is a palindrome number");
+    }
+    else {
+        printf("%d is not a palindrome number");
+    }
+}
