305938161b121ec5d2b462d4a96f8bf0945d8f55
Thanvi-27 <thanvikummithi@gmail.com>
2024-11-06 22:35:13 +0530

    updated

--- C Programming/days_in_a_month.c
+++ C Programming/days_in_a_month.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main() {
     int month;
+
     printf("enter a month:");
     scanf("%d",&month);
+    
     if(month==1||month==3||month==5||month==7||month==8||month==10||month==12){
         printf("31 days in a month");
     }
