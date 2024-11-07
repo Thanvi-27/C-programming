53e0145327e2eea3f5a57e5be34ceba2390ad445
Thanvi-27 <thanvikummithi@gmail.com>
2024-11-07 11:58:30 +0530

    File

--- C Programming/palindrome_number.c
+++ C Programming/palindrome_number.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main() {
-    int num,rev=0,rem;
+    int num,rev=0,rem,temp;
     printf("enter a number:");
     scanf("%d",&num);
-    while(num>0){
-        rem=num%10;
+    temp=num;
+    while(temp>0){
+        rem=temp%10;
         rev=rev*10+rem;
-        num=num/10;
+        temp=temp/10;
 
     }
     if(num==rev){
