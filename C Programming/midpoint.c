a80cd48d9726eb2139448eff2d17f7c2505b2e53
Thanvi-27 <thanvikummithi@gmail.com>
2024-11-08 23:57:38 +0530

    File

--- C Programming/midpoint.c
+++ C Programming/midpoint.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    float x1,y1,x2,y2,avg1,avg2;
+    float x1,y1,x2,y2,midpoint1,midpoint2;
     printf("enter x1 value:");
     scanf("%f",&x1);
      printf("enter y1 value:");
@@ -9,9 +9,9 @@ int main() {
     scanf("%f",&x2);
      printf("enter y2 value:");
     scanf("%f",&y2);
-    avg1=(x1+x2)/2;
-    avg2=(y1+y2)/2;
-    printf("Arun's house is located at (%.1f,%.1f)",avg1,avg2);
+    midpoint1=(x1+x2)/2;
+    midpoint2=(y1+y2)/2;
+    printf("Arun's house is located at (%.1f,%.1f)",midpoint1,midpoint2);
     
 }
     
